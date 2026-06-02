8cc813f983406d88e9c990ca61eaad7acd06d059
pradyumna100903 <pradyumnabhat2003@gmail.com>
2026-06-02 13:22:40 +0530

    Added JS Learning Daywise

---
+++ Day01/script.js
@@ -0,0 +1,47 @@
+console.log("Hello Praddy");   // To Print in Console 
+
+console.warn('This is Warning');   // TO give a warning in console 
+
+console.error('there is some error in downloading');   // To Give an error in the console 
+
+
+console.table(['harsh' , 'praddy' , 'ritika']);    // Prints a table along with indexes in the console window 
+
+var a = 33;
+console.log(a);
+var b = 34;
+console.log(b);
+
+console.log(a,b);
+ 
+
+// Arithmetic Operations 
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+
+// Assignment Operators 
+
+let x = 10;
+x+= 5 // x = x+5 
+x-= 5 // x = x-5 
+x*= 5 // x = x*5 
+x/= 5 // x = x/5 
+x%= 5 // x = x%5 
+
+// Comparision Operators.
+
+console.log(5=="5"); // true -> loose equality - Converts types and checks  
+console.log(5==="5"); // false -> strict equality - checks type AND values   
+console.log(5!="5"); // false -> loose equality - Converts types and checks  
+console.log(5!=="5"); // true -> strict equality - Converts types and checks  
+console.log(5=="5"); // true -> loose equality - Converts types and checks  
+
+
+
+
+
+
---
+++ Day02/script.js
@@ -0,0 +1,60 @@
+// console.log("Hello World ");
+// console.log("Day 02 ");
+
+// var a = 10;
+// var b = 20;
+
+// console.log(a+b);  //Addition
+// console.log(a-b);   // Subtraction 
+// console.log(a*b);   // Multiplication or product
+// console.log(a/b);   // Division returning quotient  
+// console.log(a%b);   // Modulus returning remainder
+// console.log(a**b);  // calculates a power b (expoential )
+
+
+
+var a = 10;
+
+// console.log(++a); // Pre Increment -> First Increment the Value by 1 then print .
+
+
+// console.log(a++); // Post Increment -> First Print the Value then increment by 1 . 
+
+console.log(--a); // Pre Decrement -> First Decrement the value by 1 then print . 
+console.log(a);
+console.log(a--); // First Print the value then Decrement by 1 
+console.log(a); 
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
