c8aa5c36b6183305685560e7884115fb27102b58
pradyumna100903 <pradyumnabhat2003@gmail.com>
2026-06-04 17:32:42 +0530

    Added Sheets and Day 03

---
+++ JS Practice Sheets Solutions/JS Beginner Solutions/script.js
@@ -0,0 +1,15 @@
+// Console and Basics 
+
+  console.log("Hello JavaScript ") ; 
+
+console.log("Hello my name is Pradyumna , I am currently 22 years old and my native city is Udupi " );
+
+console.warn("This is Deprecated and not supported ");
+
+console.error("Invalid Connection request ");
+
+console.table([1,2,3,4,5]);
+
+
+
+// Variables 
