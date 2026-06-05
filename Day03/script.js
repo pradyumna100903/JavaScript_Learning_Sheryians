d749ebf2ff053dcc0352468250b4975b25fec83c
pradyumna100903 <pradyumnabhat2003@gmail.com>
2026-06-05 19:46:49 +0530

    Added Day 03 Code Examples

---
+++ Day03/script.js
@@ -0,0 +1,99 @@
+// console.log("Phase 3 ");
+
+
+// console.log("Before if");
+// if(10>5){
+// console.log(" if block executed");
+// }
+
+// console.log('outside if ');
+
+
+// Loops - Executing some code for a specific number of times for a given condition 
+
+
+//  4 types of loops in JS - 
+//  - while 
+//  - do while
+// - for 
+// - for each
+
+// Infinite Loops using While 
+
+// while(10>5){
+//     console.log("Hello ");
+    
+// }
+
+
+//  var a = 0;
+// console.log('loop ke pehle');
+
+//  while(a<5){
+//     console.log('Hello');
+//     a++;
+//  } 
+
+// console.log('loop ke baad ');
+
+// Print Numbers in Descending Order 
+
+// var a = 10; 
+// while (a > 0)
+// {
+//     console.log(a);
+//     a--
+// }
+
+// To Print Numbers till a range as given by user 
+
+// var num = Number(prompt('Enter the Number '));
+
+// var a = 0 ;
+
+// while(a<=num)
+// {
+//     console.log(a);
+//     a++;
+    
+// }
+
+// To print even numbers 
+
+// var num = Number(prompt('Enter the Number '));
+
+// var a = 1;
+
+// while(a<=num)
+// {
+//     if(a%2==0){
+//         console.log(a);
+        
+//     }
+//     a++
+// }
+
+// Print Table of  Number given by user 
+
+// var num = Number(prompt('Enter The Number'));
+
+// var a = 1 
+// while(a<=10) {
+
+//     console.log(num+ " X " + a + " = " +num*a);
+//     a++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
