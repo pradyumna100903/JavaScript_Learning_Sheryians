c87f34c8f01ad7e0bc89877c673474c6e34baa26
pradyumna100903 <pradyumnabhat2003@gmail.com>
2026-06-09 19:53:40 +0530

    Added Math Methods Notes

--- Day03/script.js
+++ Day03/script.js
@@ -187,6 +187,23 @@ console.log(s[0]);                  // "H" (also works)
 // console.log(x); // "HELLO"
 
 
+// //  The Math Object
+
+// // `Math` is a built-in object with mathematical methods and constants.
+
+
+// console.log(Math.PI);            // 3.14159...
+// console.log(Math.E);             // 2.71828...
+
+// console.log(Math.round(4.6));    // 5
+// console.log(Math.floor(4.9));    // 4 (always rounds down)
+// console.log(Math.ceil(4.1));     // 5 (always rounds up)
+// console.log(Math.abs(-7));       // 7
+// console.log(Math.max(1, 5, 3));  // 5
+// console.log(Math.min(1, 5, 3));  // 1
+// console.log(Math.pow(2, 10));    // 1024
+// console.log(Math.sqrt(16));      // 4
+// console.log(Math.random());      // random number between 0 and 1
 
 
 
