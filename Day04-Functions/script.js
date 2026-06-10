51a4a010e0f901ab6b5082d0df3b9f4081e46863
pradyumna100903 <pradyumnabhat2003@gmail.com>
2026-06-10 13:45:32 +0530

    Added Functions (Phase 2) introduction

---
+++ Day04-Functions/script.js
@@ -0,0 +1,32 @@
+console.log("Hello JS");
+
+// Functions - These are accessible , Reusable and Systematic block of code . To declare a function we use 'function ' keyword . 
+ 
+// ex: function abc(parameter1, parameter2 , .....){
+
+// console.log('hello all');
+
+
+// }
+
+// A function only executes when called. Here greet() indicates calling the function ;
+
+// A function can be called any number of times and in any order .
+
+// a method has parameters (Here 'name' is a parameter);
+// function greet(name) {
+//     console.log("Hello..." + name);
+// }
+
+// We can pass arguments in the function calling .
+// ex: greet("Pradyumna ");  // Hello... Pradyumna 
+
+// greet();   // Hello...undefined
+
+
+// greet("Pradyumna ");  // Hello... Pradyumna 
+// greet("Praddy");
+// greet('Harsh');
+
+
+
